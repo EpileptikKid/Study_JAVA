55be54fc2b320e67128ac1e56168af9478c1dc9c
EpileptikKid <k1d.ep1lept1k@gmail.com>
2022-06-15 00:52:21 +0300

    Completed lesson_6 (is over)

--- src/Task_1/MyFunction.java
+++ src/Task_1/MyFunction.java
@@ -3,7 +3,16 @@ package Task_1;
 
 
 public class MyFunction {
+
+    public static double f(double x) {
+        return x * x - 2;
+    }
+
     public static void main(String[] args) {
         System.out.printf("%3f", MinFunc.minFunc(-2, 2, 0.00001, x -> (x + 1) * (x + 1)));
+        System.out.println();
+        System.out.printf("%3f", MinFunc.minFunc(-2, 2, 0.00001, MyFunction::f));
+        System.out.println();
+        System.out.printf("%3f", MinFunc.minFunc(-2, 2, 0.00001, Math::sin));
     }
 }
