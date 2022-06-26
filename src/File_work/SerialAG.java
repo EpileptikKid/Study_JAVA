fd46d1ce06375ff1bfd24ecb43a5c46859c1f9bb
EpileptikKid <k1d.ep1lept1k@gmail.com>
2022-06-26 11:24:17 +0300

    task 4

---
+++ src/File_work/SerialAG.java
@@ -0,0 +1,33 @@
+package File_work;
+
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class SerialAG {
+    public static void main(String[] args) {
+        AcademicGroup ag = new AcademicGroup();
+        Student st1 = new Student();
+        Student st2 = new Student();
+        Student st3 = new Student();
+        st1.setName("Vova");
+        st1.setCourse(1);
+        st2.setName("Dima");
+        st2.setCourse(2);
+        st3.setName("Misha");
+        st3.setCourse(3);
+        ag.addStudent(st1);
+        ag.addStudent(st2);
+        ag.addStudent(st3);
+        ag.setName("Super Group");
+
+        try (ObjectOutputStream out = new ObjectOutputStream(Files.newOutputStream(Paths.get("src/File_work/file.dat")))) {
+            out.writeObject(ag);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
---
+++ src/File_work/Student.java
@@ -0,0 +1,30 @@
+package File_work;
+
+import java.io.Serializable;
+
+public class Student implements Serializable{
+    private static final long serialVersionUID = -7421197435499381461L;
+    private String name;
+    private int course;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCourse() {
+        return course;
+    }
+
+    public void setCourse(int course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "name - " + getName() + ", course - " + getCourse() + ";\n";
+    }
+}
